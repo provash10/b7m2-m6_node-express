f9ec619fc26349a2d31682f344ae9c49864266e1
Provash <provash20cb@gmail.com>
2026-05-12 21:30:54 +0600

    6-11 Working with Environment-based Configurations env port 8000 config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 import { routeHandler } from "./routes/route";
+import config from "./config";
 
 const server : Server = createServer((req:IncomingMessage, res: ServerResponse)=>{
     console.log(req);
@@ -14,8 +15,12 @@ const server : Server = createServer((req:IncomingMessage, res: ServerResponse)=
 
 });
 
-
-server.listen(5000,()=>{
-    console.log("Server is running on the port 5000");
+server.listen(config.port,()=>{
+    console.log(`The Server is running on the port ${config.port}`);
     
 })
+
+// server.listen(5000,()=>{
+//     console.log("Server is running on the port 5000");
+    
+// })
